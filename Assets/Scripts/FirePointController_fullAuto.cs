2e531f3ed5a669c80a095f7e84ef8f03b984fa30
E-Eshbaugh <eeshbaugh@mines.edu>
2025-02-27 13:21:06 -0700

    forgot to add new files

---
+++ Assets/Scripts/FirePointController_fullAuto.cs
@@ -0,0 +1,9 @@
+using System.Data.Common;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirePointController : MonoBehaviour
+{
+   
+}
---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,51 @@
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootingController : MonoBehaviour
+{
+    [SerializeField]
+    public bool shooting = false;
+    public Transform FirePoint;
+    public GameObject Bullet;
+    public Animator animator;
+    public RuntimeAnimatorController newController;
+
+    private bool _isShooting = false;
+    public bool isShooting { get{return _isShooting;} private set 
+        {
+            _isShooting = value;
+            animator.SetBool("isShooting", _isShooting);
+        } 
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+        
+        if(newController != null) {
+            animator.runtimeAnimatorController = newController;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.current.rightTrigger.isPressed)
+        {
+            isShooting = true;
+            shooting = true;
+            
+            Shoot();
+        } else{
+            isShooting = false;
+            shooting = false;
+        }
+    }
+
+    void Shoot() 
+    {
+        Instantiate(Bullet, FirePoint.position, FirePoint.rotation);
+    }
+}
