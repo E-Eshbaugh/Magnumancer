a9a2bcf0647202e2fb712398ea318f45f611d445
E-Eshbaugh <eeshbaugh@mines.edu>
2025-08-08 14:44:03 -0600

    VFX on goblin spawn and VFX adn camera shake on goblin death

--- Assets/Scripts/Goblin/GoblinDeathTracker.cs
+++ Assets/Scripts/Goblin/GoblinDeathTracker.cs
@@ -4,11 +4,15 @@ public class GoblinDeathTracker : MonoBehaviour
 {
     public GoblinSpawner spawner;
 
+    [Header("Camera Shake On Death")]
+    public float shakeIntensity = 0.4f;
+    public float shakeDuration = 0.15f;
+
     void OnDestroy()
     {
         if (spawner != null)
-        {
             spawner.NotifyEnemyDeath(gameObject);
-        }
+
+        CameraShake.Shake(shakeIntensity, shakeDuration);
     }
 }
